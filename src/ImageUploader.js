74750952061ea5c049ab15ce796515f62ad1586b
Zerothlaw0095 <131967635+Zerothlaw0095@users.noreply.github.com>
2023-09-25 21:43:25 +0530

    Update ImageUploader.js

--- src/ImageUploader.js
+++ src/ImageUploader.js
@@ -1,1 +1,21 @@
- 
+ import React, { useState } from 'react';
+
+const ImageUploader = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    setSelectedImage(file);
+  };
+
+  return (
+    <div>
+      <input type="file" accept="image/*" onChange={handleImageChange} />
+      {selectedImage && <img src={URL.createObjectURL(selectedImage)} alt="Selected" />}
+    </div>
+  );
+};
+
+export default ImageUploader;
+
+
