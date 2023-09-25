40681be35c329aacf8e0650a0deba113f60a92a2
Zerothlaw0095 <131967635+Zerothlaw0095@users.noreply.github.com>
2023-09-25 21:41:49 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,24 @@
- 
+ import React, { useState } from 'react';
+import './App.css';
+import ImageUploader from './components/ImageUploader';
+import DiagnosisResult from './components/DiagnosisResult';
+
+function App() {
+  const [diagnosisResult, setDiagnosisResult] = useState(null);
+
+  const handleDiagnosis = (imageData) => {
+    setDiagnosisResult('Dermatological condition: Eczema');
+  };
+
+  return (
+    <div className="App">
+      <h1>Dermatology AI Tool</h1>
+      <ImageUploader onDiagnose={handleDiagnosis} />
+      {diagnosisResult && <DiagnosisResult result={diagnosisResult} />}
+    </div>
+  );
+}
+
+export default App;
+
+
