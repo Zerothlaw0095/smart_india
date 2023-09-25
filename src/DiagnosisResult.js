f7db7d7c272ea89c9730b1e94607172012bdbede
Zerothlaw0095 <131967635+Zerothlaw0095@users.noreply.github.com>
2023-09-25 19:55:27 +0530

    Add files via upload

---
+++ src/DiagnosisResult.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const DiagnosisResult = ({ result }) => {
+  return (
+    <div>
+      <h2>Diagnosis Result</h2>
+      <p>{result}</p>
+    </div>
+  );
+};
+
+export default DiagnosisResult;
+
